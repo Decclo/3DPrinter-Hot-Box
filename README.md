6c6546d1a8439df7a7144f11b05aeb24cf21c158
Hans V. Rasmussen <angdeclo@gmail.com>
2023-01-28 16:14:39 +0100

    Add project files.

--- README.md
+++ README.md
@@ -1,2 +1,110 @@
-# 3DPrinter-Hot-Box
-Controlled environment for 3D Printers.
+An enclosure meant to house a resin 3D printer, holding the temperature at a stable value to ensure the best results. The box allows for the printer inside to be placed in a cold and even freezing environment, such as a shed or porch, to keep potential toxic fumes away from the usual living area.
+
+Visual Studio Code from Microsoft with PlatformIO was used as IDE, though the project can be run using PlatformIO Core with no IDE.
+
+# Components needed:
+1. An isolated box, in this case with a wooden frame with a double layered plexiglass front panel, removable top, and styrofoam on the inside.
+2. [A power supply which covers all our power requirements](https://www.amazon.de/gp/product/B07BLR16PB/ref=ppx_yo_dt_b_asin_title_o00_s00?ie=UTF8&psc=1).
+3. [Arduino Nano](https://www.banggood.com/Geekcreit-ATmega328P-Nano-V3-Module-Improved-Version-No-Cable-Development-Board-Geekcreit-for-Arduino-products-that-work-with-official-Arduino-boards-p-959231.html?cur_warehouse=CN&rmmds=search) microcontroller.
+4. A heating unit, in this case a [150W 12V DC PTC Fan Heater](https://www.banggood.com/150W-12V-DC-PTC-Fan-Heater-Constant-Temperature-With-Connection-Cable-p-1120251.html?cur_warehouse=CN&rmmds=search) with integrated fan.
+5. A motor controller to take care of the fan, such as the [L298N Dual H bridge motor driver](https://www.banggood.com/Wholesale-L298N-Dual-H-Bridge-Stepper-Motor-Driver-Board-p-42826.html?cur_warehouse=CN&rmmds=search), which also features a 5v voltage regulator.
+6. A [relay](https://www.banggood.com/3Pcs-5V-Relay-5-12V-TTL-Signal-1-Channel-Module-High-Level-Expansion-Board-p-1178211.html?cur_warehouse=CN&rmmds=search) to turn the heating element on and off.
+7. We'll also need some temperature sensors for feedback, my personal favorite being the [DS18B20 OneWire digital temperature probes](https://www.banggood.com/DS18B20-Waterproof-Digital-Temperature-Temp-Sensor-Probe-1M-2M-3M-5M-10M-15M-p-1211828.html?cur_warehouse=CN&ID=45763&rmmds=search).
+8. At last, we'll need some assorted cables and soldering skills. A power socket rail would also be neat so we have easy power access for the printer.
+
+# Assembly
+The box really depends on your tastes and needs. We made the box of plywood and isolated it using polysterene. On the front we installed handles, a double-layer plexiglass window, and a closing mechanism to hold it in place. The top was made to be removable, so it would be easier to handle the 3D printer. This leaves the sides, bottom, and back as one module.
+
+We mounted the electronics on a backplate on the inside as to protect them from the outside environment. This is usually considered a bad practice, but we deemed it acceptable as the box is meant to house electronics and likely will never exceed 30 degrees celcius anyway.
+
+
+![signal-2021-03-24-194901_002](https://user-images.githubusercontent.com/25606661/112367431-30895d80-8cda-11eb-955a-b8b967ac7dbb.jpeg)
+![signal-2021-03-24-194901_003](https://user-images.githubusercontent.com/25606661/112367433-3121f400-8cda-11eb-8844-0a34f85c6d95.jpeg)
+
+The power supply is connected in the following way:
+```
+Power supply (12v output)
+|-> L298N motor driver
+| |-> Fan (from motor A out)
+| |-> Relay (from L298N 5v relugated output)
+| |-> DS18B20 temperature sensors (from L298N 5v relugated output)
+|-> Arduino (VIN)
+|-> Relay (switch)
+| |-> Heating unit
+```
+
+While the positive rail can be wired as stated above, there are no limits for the ground rail. Preferable every component would connect to every other components ground, but since this would be cumbersome, the components which give signals to other components have an additional ground wire along the signal wire. This results in a mesh-like ground rail.
+
+It should also be noted that the 12v input connected to the Arduino Nano's VIN is made with a removable connector, as to be able to change the power source from the power supply and to a computer to be able to upload code to the arduino without risking the power flowing the wrong way through the device and potentially burning the computers' USB port.
+
+**Always make sure the arduino's vin pin is not connected when connecting it to the computer using USB!**
+
+The devices are connected to the Arduino Nano as follows:
+  - Pin 10: Onewire (additionally has 4.7kOhm pullup resistor to arduino's 5v rail)  
+  - Pin 09: L298N Motor driver PWM for fan  
+  - Pin 11: Digital output for relay controlling heating element
+
+Note that the pins referred to are the ones marked with purple ont he image below.
+
+![Arduino Nano Pinout from Wikipedia.com](https://upload.wikimedia.org/wikipedia/commons/e/e4/Arduino-nano-pinout.png)
+*From wikipedia.com*
+
+![signal-2021-03-24-194901_001](https://user-images.githubusercontent.com/25606661/112367429-2ff0c700-8cda-11eb-82a0-a78cce96e969.jpeg)
+![signal-2021-03-27-220648](https://user-images.githubusercontent.com/25606661/112734892-d3471380-8f48-11eb-9a5a-19a19f92f553.jpeg)
+
+  
+# Repository Setup
+This repository was made with VSCode in mind, though platformIO has been used with the core in mind, meaning that compiling and flashing can be done using `pio` from the terminal.
+
+
+  1. Download this repository if you haven't done so already.
+  2. Initialize the required modules by running `git submodule init && git submodule update` in either your local terminal or VSCode's terminal (requires you to be in the repository directory)
+  3. Either install PlatformIO in VSCode as an extension, or install the [PlatformIO Core](https://docs.platformio.org/en/stable/core/installation/methods/index.html) using any available method.
+
+# Compiling and Flashing
+## VSCode
+You can compile the code from VSCode by pressing the 'PlatformIO:Build' button on the blue bar at the bottom. Likewise, flashing can be done by pressing the right-pointing-arrow next to the ckeckmark. Board and uplaod preferences can be changed in `platformio.ini`.
+
+
+## PlatformIO Core
+To use the Pio Core, first navigate to the workdirectory.
+
+Compiling:
+```
+pio run -e pro16MHzatmega328
+```
+
+Uploading:
+```
+pio run -e pro16MHzatmega328 --target upload
+# or:
+pio run -e pro16MHzatmega328 --target upload --upload-port /dev/ttyUSB0
+```
+
+Clean build:
+```
+pio run -e pro16MHzatmega328 --target clean
+```
+
+Monitor:
+```
+pio device monitor
+```
+
+As with VSCode, additional boards and preferences can be set in the file called `platformio.ini`.
+
+# Tests
+Two tests were made. 
+
+## Test 01 - Empty Box
+The first test was without the printer inside, meaning that only the raspberry pi overseeing the microcontroller and the heating system are producing heat. As one can see in the plots below, the heating element is only in use around 10% of the time with and outside temperature of <10C. The temperature inside the box is varying with around +-1C (from 26C to 28C) with a mean of 27.07C. This is using a desired temperature of 27C with an offset of -0.4C and an allowed deviation of 0.2C.
+![2021-03-27T10_empty_test](https://user-images.githubusercontent.com/25606661/112734271-fc65a500-8f44-11eb-9e64-c7fa682b964e.png)
+
+
+## Test 02 - Live run with printer inside
+The second test was with the printer inside, printing a batch of objects. The second test was using the same settings as Test 01, but as can be seen in the image below the printer was producing enough heat to prevent the box from cooling down, meaning that the temperature control only was used for bringing the box to operational temperature but not to keep it hot. This raises concern as the box might oberheat with higher outside temperatures or if the printer or other components might produce more heat. Possible solutions are to partly open the door to let some cold air in, or perhaps introducing some kind of cooling mechanism.
+It should be noted that the sudden spikes at ~6000s and ~9000s are caused by opening the box to check the print, with the small spike at ~600s being the act of opening the box and pressing the start button.
+![2021-03-27T14_First_printer_test](https://user-images.githubusercontent.com/25606661/112734319-4f3f5c80-8f45-11eb-8d3c-6685207f6c76.png)
+
+
+
---
+++ src/main.cpp
@@ -0,0 +1,469 @@
+/*
+ * Heat Regulated Box 
+ * 
+ * This code works as a controller for box that is to keep its internal
+ * temperature stable.
+ * 
+ * Author:        Hans Rasmussen
+ * E-Mail:        angdeclo@gmail.com
+ * Creation Date: 09/Oct/2020
+ */
+
+
+
+// ##########################################
+// ##              Includes                ##
+// ##########################################
+#include <Arduino.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include "ArduinoJson.h"
+
+
+
+// ##########################################
+// ##               Settings               ##
+// ##########################################
+/*
+ * This section defines the settings for the program, such as pin numbers and
+ * operation modi.
+ * For tuning the device, one would change DESIRED_TEMPERATURE and 
+ * TEMPERATURE_DEVIATION. As the heating element is rather quick to heat up 
+ * but needs a lot of time to cool down, the TEMPERATURE_OFFSET should be set 
+ * to the difference between the wanted value and average measured value.
+ * If the measured value is lower than the wanted, then TEMPERATURE_OFFSET 
+ * needs to be positive.
+*/
+
+// Desired temperature in degrees celsius.
+const double DESIRED_TEMPERATURE = 27;
+
+// How much deviation (+-DESIRED_TEMPERATURE) is allowed before correcting.
+const double TEMPERATURE_DEVIATION = 0.2;
+
+// Offset from the desired temperature and average measured temperature, 
+// found by using the Graph_Plotter.py
+const double TEMPERATURE_OFFSET = -0.4;
+
+// Pin for onewire interface. The sensors are installed in series running 
+// without parasitic power.
+#define ONE_WIRE_BUS 10
+
+// Precision of the temperature sensor
+// 09 equals a precision of 0.5C and measurement time <93.75ms
+// 10 equals a precision of 0.25C and measurement time <187.5ms
+// 11 equals a precision of 0.125C and measurement time <375ms
+// 12 equals a precision of 0.0625C and measurement time <750ms
+#define TEMPERATURE_PRECISION 12
+
+// Pin on which to run the PWM for the motor controller in charge of the fan. 
+// The H-bridge is hard-wired.
+#define FAN_PWM 9
+
+// Pin controlling the relay in charge of the heating element.
+#define HEAT_RELAY_PIN 11
+
+
+// ##########################################
+// ##   Objects and Function Declaraions   ##
+// ##########################################
+
+// Onewire initialization and sensor setup
+OneWire oneWire(10);  // on pin 10 (a 4.7K pull-up resistor is necessary)
+DallasTemperature sensors(&oneWire);
+
+// Addresses for the sensors
+DeviceAddress sensor0 = {0x28, 0x69, 0xA1, 0x69, 0x35, 0x19, 0x01, 0x5D};
+DeviceAddress sensor1 = {0x28, 0xD5, 0x55, 0x6B, 0x35, 0x19, 0x01, 0x99};
+DeviceAddress sensor2 = {0x28, 0x96, 0x5F, 0xE3, 0x22, 0x20, 0x01, 0x62};
+
+// Function that takes over the main loop and gives full control of the device.
+void debugMode();
+
+// Function to read out onewire addresses 
+void printAddress(DeviceAddress deviceAddress);
+
+// Function to print the temperature for a onewire device
+void printTemperature(DeviceAddress deviceAddress);
+
+// Function to print the address and temperature for a onewire device
+void printData(DeviceAddress deviceAddress);
+
+// Function which searches the OneWire bus and prints results
+uint8_t findDevices(int pin);
+
+
+
+// ##########################################
+// ##           Global Variables           ##
+// ##########################################
+/*
+ * This section contains the global variables. It would be a good idea to 
+ * change these global variables to local ones in the future, but they're 
+ * here now.
+*/
+
+bool relay_state = false;   // Bool which saves the current state of the relay, 
+                            // mostly for logging purposes.
+uint8_t fan_value = 220;    // Value to be used when changin the fan speed, 
+                            // as it makes sure the correct value is logged later.
+const double temperature_setpoint = DESIRED_TEMPERATURE + TEMPERATURE_OFFSET;
+
+
+// ##########################################
+// ##                Setup                 ##
+// ##########################################
+void setup()
+{
+    // Initializations //
+
+    // Delay to allow us to turn on any logging scripts that we might have.
+    delay(5000);
+
+    // ==== Serial ====
+    Serial.begin(115200);
+    Serial.print("Serial initialized!\n\n");
+
+
+    // ==== OneWire sensors ====
+    Serial.print("Initializing OneWire sensors...\n");
+    sensors.begin();
+
+    // locate devices on the bus
+    Serial.print("Locating devices...");
+    Serial.print("Found ");
+    Serial.print(sensors.getDeviceCount(), DEC);
+    Serial.println(" devices.");
+    Serial.println();
+
+    // report parasite power requirements
+    Serial.print("Parasitic power is ");
+    if (sensors.isParasitePowerMode())
+    {
+      Serial.println("ON");
+    }
+    else 
+    {
+      Serial.println("OFF");
+    }
+
+    // show the addresses we found on the bus
+    Serial.print("Device 0 Address: ");
+    printAddress(sensor0);
+    Serial.println();
+    Serial.println("Device 0 Description: Inside, low");
+
+    Serial.print("Device 1 Address: ");
+    printAddress(sensor1);
+    Serial.println();
+    Serial.println("Device 1 Description: Inside, high");
+
+    Serial.print("Device 2 Address: ");
+    printAddress(sensor2);
+    Serial.println();
+    Serial.println("Device 2 Description: Outside, reference");
+    Serial.println();
+
+    // set the resolution to 12 bit per device, this results in <750ms reading time per sensor with resolution 0.0625C
+    sensors.setResolution(sensor0, TEMPERATURE_PRECISION);
+    sensors.setResolution(sensor1, TEMPERATURE_PRECISION);
+    sensors.setResolution(sensor2, TEMPERATURE_PRECISION);
+    Serial.println();
+
+    Serial.print("Device 0 Resolution: ");
+    Serial.print(sensors.getResolution(sensor0), DEC);
+    Serial.println();
+
+    Serial.print("Device 1 Resolution: ");
+    Serial.print(sensors.getResolution(sensor1), DEC);
+    Serial.println();
+
+    Serial.print("Device 2 Resolution: ");
+    Serial.print(sensors.getResolution(sensor2), DEC);
+    Serial.println();
+    Serial.println("OneWire sensors initialized!");
+    Serial.println();
+
+    // ==== Fan Control ====
+    // Initialize PWM pin as PWM output
+    pinMode(FAN_PWM, OUTPUT);
+
+    // ==== Heat Relay Control ====
+    // Initialize IO pin as output
+    pinMode(HEAT_RELAY_PIN, OUTPUT);
+
+    // Give advice on how to enter debugging more, and wait a second so the user will have chance to read it.
+    Serial.println("For debugMode please press anykey.");
+    Serial.println();
+    delay(1000);
+
+    // Turn on the fan. For now we'll just keep it constant.
+    analogWrite(FAN_PWM, fan_value);
+
+}
+
+
+// ##########################################
+// ##                 Main                 ##
+// ##########################################
+void loop()
+{
+    // Check if the user wants to enter debugmode
+    if (Serial.available() > 0)
+    {
+      debugMode();
+    }
+
+    // Read all the sensors, and find the mean of the inside sensors.
+    sensors.requestTemperatures();
+    float tempC_S0 = sensors.getTempC(sensor0);
+    float tempC_S1 = sensors.getTempC(sensor1);
+    float tempC_S2 = sensors.getTempC(sensor2);
+    float tempC_mean = ((tempC_S0+tempC_S1)/2);
+    
+    // This is the actual control algorithm. 
+    // For now it simply turns the heating element on when the temperature falls below a deviation defined by the user, 
+    // and turns it off again once it surpasses this same deviation
+    if (tempC_mean <= (temperature_setpoint - TEMPERATURE_DEVIATION))
+    {
+      digitalWrite(HEAT_RELAY_PIN, HIGH);
+      relay_state = true;
+    }
+    else if (tempC_mean >= (temperature_setpoint + TEMPERATURE_DEVIATION))
+    {
+      digitalWrite(HEAT_RELAY_PIN, LOW);
+      relay_state = false;
+    }
+
+    // Debugging and logging - Creates a JSON document and sends it over Serial
+    // This piece of code is mostly autogenerated using the ArduinoJson Assistant: https://arduinojson.org/v6/assistant/
+    // The following piece of JSON is used in step 2:
+    /*
+    {
+    "ms": 1000000,
+    "sensors": [
+    {
+      "sensor01": 00.000,
+      "sensor02": 00.000,
+      "sensor03": 00.000
+    }
+    ],
+    "sensorMean": 00.000,
+    "fan": 255,
+    "heatingElement": 0
+    }
+    */
+    StaticJsonDocument<96> doc;
+
+    doc["ms"] = millis();
+    JsonObject sensors_0 = doc["sensors"].createNestedObject();
+    sensors_0["sensor00"] = tempC_S0;
+    sensors_0["sensor01"] = tempC_S1;
+    sensors_0["sensor02"] = tempC_S2;
+    doc["sensorMean"] = tempC_mean;
+    doc["fan"] = fan_value;
+    doc["heatingElement"] = relay_state;
+
+    serializeJson(doc, Serial);
+    Serial.println();
+}
+
+
+
+// ##########################################
+// ##               Functions              ##
+// ##########################################
+
+// Function that takes over the main loop and gives full control of the device.
+void debugMode()
+{
+    int debugmode_enabled = true;
+    char rx_byte = 0;
+
+    // Clear any input we might have
+    if (Serial.available() > 0) 
+    {
+        rx_byte = Serial.read();
+    }
+
+    // Print the table of choices, read the input, and decide on what to do
+    while (debugmode_enabled)
+    {
+        Serial.println("\nCommands:\n");
+        Serial.println("\t1 - Read temperature\n");
+        Serial.println("\t2 - Set PWM\n");
+        Serial.println("\t3 - Change relay state to on\n");
+        Serial.println("\t4 - Change relay state to off\n");
+        Serial.println("\t5 - Scan for OneWire sensors\n");
+        Serial.println("\t0 - Exit debugMode and return to normal operation\n");
+        Serial.println("Choice: ");
+        
+        // Wait for user input
+        while(Serial.available() == 0)
+        {
+            delay(100);
+        }
+
+        // Once the user has sent a command, read and save it. This does not actually need an if-stament, but I like making safeties.
+        if (Serial.available() > 0)     // is a character available?
+        {    
+            rx_byte = Serial.read();      // get the character
+            Serial.print(rx_byte);
+            Serial.print("\n");
+
+            // check whether the recieved byte was valid, and execute the command
+            if (rx_byte == '1')
+            {
+                Serial.println("\nReading temperature sensors. Press anykey to return.");
+                bool read_temp = 1;
+                while (read_temp)
+                {
+                    sensors.requestTemperatures();
+                    printData(sensor0);
+                    printData(sensor1);
+                    printData(sensor2);
+                    Serial.println();
+
+                    // Checks for a character so that we can break free of this while loop
+                    char rx_byte_alt = 0;
+                    if (Serial.available() > 0)         // is a character available?
+                    {    
+                        rx_byte_alt = Serial.read();      // get the character
+                        read_temp = 0;
+                    }
+                }
+            }
+            else if (rx_byte == '2')
+            {
+                char rx_byte_alt = 0;
+                String rx_str_alt = "";
+                bool read_str = 1;
+
+                Serial.print("\nPlease input PWM between 100 to 255: ");
+
+                while (read_str)
+                {
+                    if (Serial.available() > 0)         // is a character available?
+                    {    
+                        rx_byte_alt = Serial.read();      // get the character
+                        
+                        if (rx_byte_alt != '\n') 
+                        {
+                            // a character of the string was received
+                            rx_str_alt += rx_byte_alt;
+                            Serial.print(rx_byte_alt);
+                        }
+                        else 
+                        {
+                            Serial.print("\nSetting pwm to ");
+                            Serial.println(rx_str_alt.toInt());
+                            Serial.println();
+                            Serial.println();
+                            analogWrite(FAN_PWM, rx_str_alt.toInt());
+                            read_str = 0;
+                        }
+                    }
+                }
+            }
+            else if (rx_byte == '3')
+            {
+                Serial.println("\nTurning on the relay...");
+                digitalWrite(HEAT_RELAY_PIN, HIGH);
+            }
+            else if (rx_byte == '4')
+            {
+                Serial.println("\nTurning off the relay...");
+                digitalWrite(HEAT_RELAY_PIN, LOW);
+            }
+            else if (rx_byte == '5')
+            {
+                findDevices(ONE_WIRE_BUS);
+                Serial.println("\n//\n// End oneWireSearch.ino //");
+                Serial.println();
+            }
+            else if (rx_byte == '0')
+            {
+                debugmode_enabled = false;
+            }
+            else 
+            {
+                Serial.println("Command not recognized!");
+            }
+        }
+    }
+
+}
+
+
+// function to print a device address
+void printAddress(DeviceAddress deviceAddress)
+{
+    for (uint8_t i = 0; i < 8; i++)
+    {
+        if (deviceAddress[i] < 16) Serial.print("0");
+        Serial.print(deviceAddress[i], HEX);
+        Serial.print(" ");
+    }
+  }
+
+
+// function to print the temperature for a device
+void printTemperature(DeviceAddress deviceAddress)
+{
+    float tempC = sensors.getTempC(deviceAddress);
+    if(tempC == DEVICE_DISCONNECTED_C) 
+    {
+        Serial.println("Error: Could not read temperature data");
+        return;
+    }
+    Serial.print("Temp C: ");
+    Serial.print(tempC);
+    Serial.print(" Temp F: ");
+    Serial.print(DallasTemperature::toFahrenheit(tempC));
+}
+
+
+// main function to print information about a device
+void printData(DeviceAddress deviceAddress)
+{
+    Serial.print("Device Address: ");
+    printAddress(deviceAddress);
+    Serial.print(" ");
+    printTemperature(deviceAddress);
+    Serial.println();
+}
+
+
+// Function which searches the OneWire bus and prints results
+uint8_t findDevices(int pin)
+{
+    OneWire ow(pin);
+
+    uint8_t address[8];
+    uint8_t count = 0;
+
+
+    if (ow.search(address))
+    {
+        Serial.print("\nuint8_t pin");
+        Serial.print(pin, DEC);
+        Serial.println("[][8] = {");
+        do {
+            count++;
+            Serial.println("  {");
+            for (uint8_t i = 0; i < 8; i++)
+            {
+                Serial.print("0x");
+                if (address[i] < 0x10) Serial.print("0");
+                Serial.print(address[i], HEX);
+                if (i < 7) Serial.print(", ");
+            }
+            Serial.println("  },");
+        } while (ow.search(address));
+
+        Serial.println("};");
+        Serial.print("// nr devices found: ");
+        Serial.println(count);
+    }
+    return count;
+}
